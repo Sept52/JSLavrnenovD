324b2ede7711fdd739a0e9959ebe16b4da3406bd
Danila Lavrenov <danila.sept@mail.ru>
2022-05-10 16:06:12 +0300

    Task3Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 // 1
 
-/* let firstNumbers = +prompt(" Введите первое число диапазона");
-let twoNumbers = +prompt(" Введите второе число диапазона");
+/* const firstNumbers = +prompt(" Введите первое число диапазона");
+const twoNumbers = +prompt(" Введите второе число диапазона");
 
 function range(num, num2) {
   let subNym = num;
   let subNym2 = num2;
   if (num2 - num >= 5 && num2 > num) {
-    for (i = 0; i <= num2 - num; i++) {
+    for (let i = 0; i <= num2 - num; i++) {
       console.log(subNym++);
     }
   } else if (num - num2 >= 5 && num > num2) {
-    for (i = 0; i <= num - num2; i++) {
+    for (let i = 0; i <= num - num2; i++) {
       console.log(subNym--);
     }
   } else {
@@ -23,11 +23,11 @@ range(firstNumbers, twoNumbers); */
 
 // 2
 
-/* let truePinCode = "0001";
-let truePukCode = "4444";
+/* const truePinCode = "0001";
+const truePukCode = "4444";
 
 function checkCode() {
-  for (i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 3; i++) {
     let enterCode = prompt("Введите PIN-код:");
     if ((enterCode === truePinCode) && !isNaN(enterCode)) {
       return alert("Вы ввели правильный PIN-код");
@@ -35,7 +35,7 @@ function checkCode() {
       alert("Введен неверный PIN-код");
     }
   }
-  for (i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 3; i++) {
     let enterCode = prompt("Введите PUK-код:");
     if (enterCode === truePukCode && !isNaN(enterCode)) {
       return alert("Вы ввели правильный PUK-код");
@@ -49,22 +49,22 @@ checkCode(); */
 
 // 3
 
-/* let firstNumbers = +prompt(" Введите первое число диапазона");
-let twoNumbers = +prompt(" Введите второе число диапазона");
+/* const firstNumbers = +prompt(" Введите первое число диапазона");
+const twoNumbers = +prompt(" Введите второе число диапазона");
 
 function range(num, num2) {
   let subNym = num;
   let subNym2 = num2;
 
   if (num2 - num >= 5 && num2 > num) {
-    for (i = 0; i <= num2 - num; i++) {
+    for (let i = 0; i <= num2 - num; i++) {
       let moreLess = (subNym > 100) ? "больше ста" : "меньше ста";
       let evenness = (subNym % 2 == 0 ) ? "чётное число" : "нечётное число";
       let positive = (subNym > 0) ? "положительное число" : "отрицательное число" ;
       console.log(`${subNym++}, ${moreLess}, ${evenness}, ${positive}.`);
     }
   } else if (num - num2 >= 5 && num > num2) {
-    for (i = 0; i <= num - num2; i++) {
+    for (let i = 0; i <= num - num2; i++) {
       let moreLess = (subNym2 > 100) ? "больше ста" : "меньше ста";
       let evenness = (subNym2 % 2 == 0 ) ? "чётное число" : "нечётное число";
       let positive = (subNym2 > 0) ? "положительное число" : "отрицательное число" ;
@@ -78,8 +78,8 @@ range(firstNumbers, twoNumbers); */
 
 // 4
 
-/* let firstNumbers = +prompt(" Введите первое число диапазона");
-let twoNumbers = +prompt(" Введите второе число диапазона");
+/* const firstNumbers = +prompt(" Введите первое число диапазона");
+const twoNumbers = +prompt(" Введите второе число диапазона");
 
 function range(num, num2) {
   let subNym = num;
@@ -87,15 +87,13 @@ function range(num, num2) {
 
 
   if (num2 > num) {
-    for (i = 0; i <= num2 - num; i++) {
-      console.log(subNym++);
+    for (let i = 0; i <= num2 - num; i++) {
       if (i === 7){
         return;
       }
     }
   } else if (num > num2) {
-    for (i = 0; i <= num - num2; i++) {
-      console.log(subNym2++);
+    for (let i = 0; i <= num - num2; i++) {
       if (i === 7){
         return;
       }
